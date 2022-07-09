1a22a89b40f5c7a425eb343bc8fe322018b6655f
김태영 <kimtaeyoung@0Taes-Mac.local>
2022-07-10 01:55:18 +0900

    #14 STREAMS

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -4,7 +4,8 @@ declare global {
   var client: PrismaClient | undefined;
 }
 
-const client = global.client || new PrismaClient();
+const client = global.client || new PrismaClient({ log: ['query'] });
+// db 쿼리를 확인할 수 있음(쿼리 크기 확인)
 
 if (process.env.NODE_ENV === 'development') global.client = client;
 
